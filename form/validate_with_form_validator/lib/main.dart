8541f0e728e2fb4357dbe82e632e2ec0ba1022e6
karan sivarat <bomb0069@gmail.com>
2022-02-27 09:30:51 +0700

    [Added]: require message to textFormFiled

--- form/validate_with_form_validator/lib/main.dart
+++ form/validate_with_form_validator/lib/main.dart
@@ -52,7 +52,8 @@ class MyCustomFormState extends State<MyCustomForm> {
         children: [
           TextFormField(
               // The validator receives the text that the user has entered.
-              validator: ValidationBuilder().build()),
+              validator:
+                  ValidationBuilder(requiredMessage: 'กรุณากรอกชื่อ').build()),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: ElevatedButton(
